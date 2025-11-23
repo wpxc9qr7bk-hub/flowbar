b12bd468fb2fa79908c303ea2b628f9f1db578c0
Rafael Lagos <rafaellagos@MacBook-Pro-2.local>
2025-11-23 17:12:57 -0300

    FiY

--- flowbar_project/settings.py
+++ flowbar_project/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ---------------------------------
 # SECURITY
 # ---------------------------------
-SECRET_KEY = config('SECRET_KEY')
+
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-clave-temporal-12345')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
